0840ce48def4953a5b9fb8c076c8bf5d6a9674ca
Nikhil Thakurdesai <34545786+nikhil1024@users.noreply.github.com>
2020-11-08 02:03:01 -0500

    Added val dataloader

--- dataloader/KITTI_submission_loader.py
+++ dataloader/KITTI_submission_loader.py
@@ -27,3 +27,36 @@ def dataloader(filepath):
   right_test = [filepath+right_fold+img for img in image]
 
   return left_test, right_test
+
+
+def dataloader_val(filepath):
+
+  left_fold  = 'image_2/'
+  right_fold = 'image_3/'
+
+
+  # image = [img for img in os.listdir(filepath+left_fold) if img.find('_10') > -1]
+
+  file_path = r"./dataloader/kitti2015_training_val_iucvg.txt"
+  with open(file_path, 'r') as f:
+    files = f.readlines()
+
+  flag = True
+  train = []
+  val = []
+  for i, file_ in enumerate(files):
+    if "Training" in file_:
+      continue
+    if "Val" in file_:
+      flag = False
+      continue
+    if ".png" in file_:
+      if flag:
+        train.append(file_.split()[0])
+      else:
+        val.append(file_.split()[0])
+
+  left_test  = [filepath+left_fold+img for img in val]
+  right_test = [filepath+right_fold+img for img in val]
+
+  return left_test, right_test
