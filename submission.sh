93bc6a89c1884d1b9e43cfeec9d1758b177ec710
Nikhil Thakurdesai <nthakurd@iu.edu>
2020-11-08 02:11:13 -0500

    Added submission bash file

---
+++ submission.sh
@@ -0,0 +1,5 @@
+python submission.py --maxdisp 192 \
+                     --model stackhourglass \
+                     --KITTI 2015 \
+                     --datapath ../data_scene_flow_2015/training/ \
+                     --loadmodel ./trained_models/finetune_190.tar \
